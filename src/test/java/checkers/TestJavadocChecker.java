09c46934f52608fd0811cb3c5fcf1462f68251ee
Jae Dong Hwang <jaedong@uw.edu>
2019-05-19 09:01:52 -0700

    UPDATED: unittest for JavadocChecker.

--- src/test/java/checkers/TestJavadocChecker.java
+++ src/test/java/checkers/TestJavadocChecker.java
@@ -2,20 +2,39 @@ package checkers;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import models.Issue;
 import org.junit.Test;
 import utils.JavadocRemover;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class TestJavadocChecker{
     
     @Test
     public void testNoJavaDocInPublicClass() throws FileNotFoundException {
-        CompilationUnit cu;
-        cu = JavaParser.parse(new File("/Users/haujd98/Documents/UW/csep590_sp19/SmartCheckstyle/source_to_parse/checkers/DocumentChecker1.java"));
-        JavadocRemover jdc = new JavadocRemover();
-        jdc.visit(cu, null);
+        
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        String fileName = classLoader.getResource("checkers/DocumentChecker1.java").getFile();
+        CompilationUnit cu = JavaParser.parse(new File(fileName));
+        JavadocRemover jdr = new JavadocRemover();
+        jdr.visit(cu, null);
+    
+        LinkedList<Issue> issues = new LinkedList<>();
+        JavadocChecker jdc = new JavadocChecker(fileName);
+        jdc.visit(cu, issues);
+        
+        Collections.sort(issues, Comparator.comparing(Issue::getLineNumber));
+        assertEquals(issues.size(), 1);
+        for (Issue issue: issues) {
+            System.out.println(issue);
+        }
+        
         
     }
 }
